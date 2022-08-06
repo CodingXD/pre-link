6f3cc1cc467bc26f11f2cd6538a3e7224221cf5b
CodingXD <marshallshelly97@gmail.com>
2022-08-07 01:36:01 +0530

    added shimmer to skeletons

--- components/skeleton/index.tsx
+++ components/skeleton/index.tsx
@@ -1,9 +1,11 @@
+import styles from "./index.module.css";
+
 interface Props {
   size?: "md" | "sm" | "xs" | "full";
 }
 
 export default function Skeleton({ size = "full" }: Props) {
-  let classes = "bg-gray-300 py-1 rounded-full";
+  let classes = "bg-gray-300 py-1 rounded-full relative overflow-hidden";
   if (size === "sm") {
     classes += " w-1/3";
   } else if (size === "md") {
@@ -13,5 +15,11 @@ export default function Skeleton({ size = "full" }: Props) {
   } else {
     classes += " w-full";
   }
-  return <div className={classes}></div>;
+  return (
+    <div className={classes}>
+      <div className={`absolute top-0 left-0 w-full h-full ${styles.wrapper}`}>
+        <div className="w-1/2 h-full bg-white/20 -skew-x-[20deg]"></div>
+      </div>
+    </div>
+  );
 }
